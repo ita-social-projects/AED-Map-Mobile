b7399a2bdf6d64b1706d886db0f6e21ec5fbc986
Ostap Kovbasiuk <ostapkovbasyk@gmail.com>
2020-02-14 11:37:42 +0200

    Base map with location

---
+++ App.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import MapboxGL from '@react-native-mapbox-gl/maps';
+
+MapboxGL.setAccessToken(
+  'pk.eyJ1Ijoib3Nrb3ZiYXNpdWsiLCJhIjoiY2s1NWVwcnhhMDhrazNmcGNvZjJ1MnA4OSJ9.56GsGp2cl6zpYh-Ns8ThxA',
+);
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  container: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'green',
+  },
+  map: {
+    flex: 1,
+  },
+});
+const someEvent = event => {
+  console.log(MapboxGL.Camera);
+};
+
+const userLocationUpdate = event => {
+  console.log(event);
+};
+const App = () => {
+  const [mapCenter, setMapCenter] = useState({
+    coords: [24.031610977781128, 49.84180396191118],
+    zoom: 13,
+    centeredOnUser: false,
+  });
+  return (
+    <View style={styles.page}>
+      <View style={styles.container}>
+        <MapboxGL.MapView style={styles.map} onLongPress={someEvent}>
+          <MapboxGL.Camera
+            centerCoordinate={mapCenter.coords}
+            zoomLevel={mapCenter.zoom}
+          />
+          <MapboxGL.UserLocation visible={true} onUpdate={userLocationUpdate} />
+        </MapboxGL.MapView>
+      </View>
+    </View>
+  );
+};
+
+export default App;
